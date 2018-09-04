0947d00a174b2f6e467a0be5c178fe5940a023f6
netx <netx@vm-devware1-tdp.nga.gov>
2018-09-04 16:01:00 -0400

    updated script to detect where it's running from

--- logmonitor/deploy_logmonitor_config.bash
+++ logmonitor/deploy_logmonitor_config.bash
@@ -6,22 +6,21 @@ if [ "$(whoami)" != "$target_user" ]; then
   exec sudo -u "$target_user" -- "$0" "$@"
 fi
 
-source /usr/local/nga/bin/netxdeploy/netx/netx_servers.config
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+source ${DIR}/../netx_servers.config
 
 if [ "$#" -lt 1 ]; then
     echo "You must specify at least one set of servers for deployment: (dev|devapp|devutil|tst|tstapp|tstutil|prod|prodapp|produtil)"
     exit 1;
 fi
 
-# cd to the proper working directory
-cd /usr/local/nga/bin/netxdeploy/netx/logmonitor
-
 for e in "$@"
 do
     serverlist=${servers[${e}]};
     for server in ${serverlist}; do 
         echo "scp ./applogmonitor.config netx@$server:/usr/local/nga/etc/applogmonitor.config"
-        scp ./applogmonitor.config netx@$server:/usr/local/nga/etc/applogmonitor.config
+        scp ${DIR}/applogmonitor.config netx@$server:/usr/local/nga/etc/applogmonitor.config
         if [ $? -ne 0 ]; then
             echo "Problem copying applogmonitor.config to $server.  Aborting";
             exit $retval
