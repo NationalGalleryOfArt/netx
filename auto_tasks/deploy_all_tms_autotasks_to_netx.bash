be4ceb8c2b3d97bf4eefeadff36e056b1c293747
beaudet <d-beaudet@BEAUDETD-W10.NGA.GOV>
2024-04-05 14:25:23 -0400

    revised autotask deployment and made additional changes to app log monitor config

--- auto_tasks/deploy_all_tms_autotasks_to_netx.bash
+++ auto_tasks/deploy_all_tms_autotasks_to_netx.bash
@@ -6,15 +6,13 @@ if [ "$(whoami)" != "$target_user" ]; then
   exec sudo -u "$target_user" -- "$0" "$@"
 fi
 
-source ../netx_servers.config
-
 if [ "$#" -lt 1 ]; then
     echo "You must specify at least one set of servers for deployment: (dev|devapp|devutil|test|testapp|testutil|prod|prodapp|produtil)"
     exit 1;
 fi
 
-
 # read secrets
+
 source /usr/local/nga/etc/tmsprivateextract.conf
 if [ "$?" -ne 0 ]; then
     echo "no config found for tmsprivateextract";
@@ -27,6 +25,22 @@ if [ "$?" -ne 0 ]; then
     exit 1;
 fi
 
+
+# Determine connection string based on environment
+case $1 in
+  prod*)
+    tmsprivate_connection_string=$tmsprivateextract_prod_connection_string
+    ;;
+  test*)
+    tmsprivate_connection_string=$tmsprivateextract_test_connection_string
+    ;;
+  dev*)
+    tmsprivate_connection_string=$tmsprivateextract_dev_connection_string
+    ;;
+  *)
+    die "no connection string for this environment"
+esac
+
 credentials_json="[\"${netx_api_username}\", \"${netx_api_password}\"]"
 
 header_json=`cat api_header.json`
@@ -38,6 +52,7 @@ datasync=`cat syncedMetadata.xml`
 # replace template with our secrets
 datasync="${datasync/tmsprivateextract_username/${tmsprivateextract_username}}"
 datasync="${datasync/tmsprivateextract_password/${tmsprivateextract_password}}"
+datasync="${datasync/connection_string/${tmsprivate_connection_string}}"
 
 mkdir -p target
 
@@ -47,11 +62,11 @@ echo "${header_json}" > ./target/auth.json
 # create the master metadatasync file that will be deployed to all servers
 echo "$datasync" > ./target/syncedMetadata.xml
 
+source ../netx_servers.config
 for e in $@; do
     serverlist=${servers[${e}]};
-    for server in ${serverlist}; do 
-echo $server
-        #echo $server
+    for server in ${serverlist}; do
+        echo $server
         echo "scp ./target/syncedMetadata.xml netx@$server:/opt/netx/netx/config/"
         scp ./target/syncedMetadata.xml netx@$server:/opt/netx/netx/config/
         if [ $? -ne 0 ]; then
@@ -59,9 +74,7 @@ echo $server
             exit $retval
         fi
 
-        #echo "curl -s -i -D /dev/null -X POST \"https://${server}.nga.gov/x7/v1.2/json/\" -H \"Content-Type: text/json\" --data-binary \"@./target/auth.json\" -o -";
         authresponse=`curl -s -i -D /dev/null -X POST "https://${server}.nga.gov/x7/v1.2/json/" -H "Content-Type: text/json" --data-binary "@./target/auth.json" -o -`
-        #echo "---${authresponse}---";
         if [[ $authresponse =~ result\":\" ]]; then
             authresponse=`echo ${authresponse} | sed 's/\(.*\)result":"\([^"]*\)"\(.*\)/\2/'`
         else
@@ -80,12 +93,10 @@ echo $server
         for task in ./tasks-to-deploy/*.xml; do
             autotask_json=`cat ${task} | jq '.' -R --slurp`
             task_json="[\"${sessionid}\", ${autotask_json}]"
-            #echo TASK:$task_json
             setautotask_json="${header_json/_params_/${task_json}}"
             fname=`echo "$task" | sed "s/.*\///"`
             echo "${setautotask_json}" > ./target/${fname}.json
             authresponse=`curl -s -i -D /dev/null -X POST "https://${server}.nga.gov/x7/v1.2/json/" -H "Content-Type: text/json" --data-binary "@./target/${fname}.json" -o -`
-            #echo $authresponse
             if [[ $authresponse =~ result\":true ]]; then
                 echo "Deployed autotask $task successfully"
             else
@@ -96,6 +107,5 @@ echo $server
     done
 done
 
-# finally, remove all contents in the target folder (working directory)
 \rm -r target
 
