217a8f150237deed537ee23714ed26cf71fbe3da
NetX <netx@BEAUDETD-W10.NGA.GOV>
2025-02-27 12:14:25 -0500

    fixed url for sts.nga.gov to adfs.nga.gov and added some log filters

--- saml/get_saml_signing_cert.bash
+++ saml/get_saml_signing_cert.bash
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+curl -s -o /tmp/adfs_saml.xml "https://adfs.nga.gov/FederationMetadata/2007-06/FederationMetadata.xml"
 /bin/echo -----BEGIN CERTIFICATE-----
-/usr/bin/curl -s -o - "https://sts.nga.gov/FederationMetadata/2007-06/FederationMetadata.xml" | /usr/bin/xmllint -format /tmp/adfs_saml.xml  | /bin/grep "<X509Certificate" | /usr/bin/head -1 | /bin/cut -d '>' -f 2 | /bin/cut -d '<' -f 1 | /usr/bin/fold -w 64
+xmllint --format /tmp/adfs_saml.xml | grep "<X509Certificate" | head -1 | cut -d '>' -f 2 | cut -d '<' -f 1 | fold -w 64
 /bin/echo -----END CERTIFICATE-----
+
+#/usr/bin/curl -s -o - "https://adfs.nga.gov/FederationMetadata/2007-06/FederationMetadata.xml" | /usr/bin/xmllint -format /tmp/adfs_saml.xml  | /bin/grep "<X509Certificate" | /usr/bin/head -1 | /bin/cut -d '>' -f 2 | /bin/cut -d '<' -f 1 | /usr/bin/fold -w 64
